23b21c29a46c1147d7402b737f71555a5ea99894
LorenzoNepi <lorenzo.nepi02@gmail.com>
2025-10-26 12:23:22 +0100

    First test for version release and tagging

--- noxfile.py
+++ noxfile.py
@@ -62,7 +62,7 @@ def ruff(session: nox.Session) -> None:
     """Run ruff.
     """
     session.install("ruff")
-    #session.install(".[dev]")
+    session.install(".[dev]")
     session.run("ruff", "check", *session.posargs)
 
 # Session to run pylint
@@ -71,7 +71,7 @@ def pylint(session: nox.Session) -> None:
     """Run pylint.
     """
     session.install("pylint")
-    #session.install(".[dev]")
+    session.install(".[dev]")
     session.run("pylint", *_LINT_DIRS, *session.posargs)
 
 # Session to run mypy
@@ -80,6 +80,7 @@ def mypy(session: nox.Session) -> None:
     """Run mypy.
     """
     session.install("mypy")
+    session.install(".[dev]")
     session.run("mypy", *_LINT_DIRS, *session.posargs )
 
 # Session tu tun unit tests
@@ -88,5 +89,5 @@ def test(session: nox.Session) -> None:
     """Run the unit tests.
     """
     session.install("pytest")
-    #session.install(".[dev]")
+    session.install(".[dev]")
     session.run("pytest", *session.posargs)
